d047dfde6e515f8bc2b07466bc729d49216996cf
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2013-06-26 10:41:41 -0400

    Skeleton imported from XIO.

---
+++ test/buster.js
@@ -0,0 +1,21 @@
+/**
+ * unit test settings for BusterJS.
+ */
+var config = module.exports;
+
+config['xio_development'] = {
+  rootPath : "../",
+  environment : "browser",
+  resources : [ "lib/google-closure-library/closure/goog/**/*.js", "src/**/*.js" ],
+  libs : [ "lib/google-closure-library/closure/goog/base.js",
+      "lib/google-closure-library/closure/goog/deps.js", "xio-deps.js",
+      "test/requires.js" ],
+  tests : [ 'test/parserNII-test.js', ]
+};
+
+config['xio_build'] = {
+  rootPath : "../",
+  environment : "browser",
+  libs : [ "bin/xio.js" ],
+  tests : [ 'test/parserNII-test.js', ]
+};
---
+++ test/parserNII-test.js
@@ -0,0 +1,16 @@
+(function() {
+
+  buster.testCase('parserNII', {
+    setUp : function() {
+
+      var parser = new X.io.parserNII();
+
+      this.parser = parser;
+
+    },
+    "classname" : function() {
+      assert(this.parser.classname, 'io.parserNIIx');
+    }
+  });
+
+})();
