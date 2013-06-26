4cdd16bee34dd7c51a988a05453064bd12881032
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2013-06-26 17:08:11 -0400

    Enable testing of X.renderer3D.

--- test/buster.js
+++ test/buster.js
@@ -12,12 +12,12 @@ config['xr_development'] = {
   libs : [ "lib/google-closure-library/closure/goog/base.js",
       "lib/google-closure-library/closure/goog/deps.js", "xr-deps.js",
       "test/requires.js" ],
-  tests : [ 'test/base-test.js', ]
+  tests : [ 'test/base-test.js', 'test/renderer3D-test.js', ]
 };
 
 config['xr_build'] = {
   rootPath : "../",
   environment : "browser",
   libs : [ "bin/xr.js" ],
-  tests : [ 'test/base-test.js', ]
+  tests : [ 'test/base-test.js', 'test/renderer3D-test.js', ]
 };
