213fb8ef7a5d672bcf63e353330e5208adc333d0
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2013-06-27 09:53:05 -0400

    Re-enable the default init test for X.renderer3D.

--- test/renderer3D-test.js
+++ test/renderer3D-test.js
@@ -32,29 +32,29 @@
       assert(this.renderer3D.width == 400);
       assert(this.renderer3D.height == 300);
     },
-//    "init" : function() {
-//
-//      // remove margin on body
-//      window.document.body.style.margin = '0px';
-//      
-//      // initialize using the <body> (default)
-//      this.renderer3D.init();
-//
-//      // now the height and width of the renderer should match the <body>
-//      assert(this.renderer3D.height == window.document.body.clientHeight);
-//      assert(this.renderer3D.width == window.document.body.clientWidth);
-//
-//      // and the canvas element should match the first child of the body
-//      assert(this.renderer3D.canvas == window.document.body.children[0]);
-//
-//    },
+    "init" : function() {
+
+      // set the container back to the default (<body>)
+      this.renderer3D.container = window.document.body;
+
+      // initialize using the <body> (default)
+      this.renderer3D.init();
+
+      // now the height and width of the renderer should match the <body>
+      assert(this.renderer3D.height == window.document.body.clientHeight);
+      assert(this.renderer3D.width == window.document.body.clientWidth);
+
+      // and the canvas element should match the first child of the body
+      assert(this.renderer3D.canvas == window.document.body.children[0]);
+
+    },
     "init with container" : function() {
       // setup the container
       var domElement = window.document.createElement('div');
       domElement.style.width = '200px';
       domElement.style.height = '100px';
       window.document.body.appendChild(domElement);
-      this.renderer3D.container = domElement;      
+      this.renderer3D.container = domElement;
 
       // initialize using the container
       this.renderer3D.init();
