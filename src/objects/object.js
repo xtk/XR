6a30636253a850d92d089bc9ee556d99ed22c1b3
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2013-07-03 16:48:45 -0400

    Restructure X.object to use the new inject functionality.

--- src/objects/object.js
+++ src/objects/object.js
@@ -1,10 +1,10 @@
 // provides
-goog.provide('X.object2');
+goog.provide('X.object');
 
 // requires
 goog.require('X.base');
-goog.require('X.buffer');
 goog.require('X.shader');
+goog.require('goog.webgl');
 
 /**
  * The superclass for all renderable objects.
@@ -12,7 +12,7 @@ goog.require('X.shader');
  * @constructor
  * @extends X.base
  */
-X.object2 = function() {
+X.object = function() {
 
   // call the superclass constructor
   X.__super__(this);
@@ -26,7 +26,9 @@ X.object2 = function() {
    * @type {!string}
    * @protected
    */
-  this._classname = 'object2';
+  this._classname = 'object';
+
+  this._gl = null;
 
   this._points = new Float32Array( [
              -1.0,  1.0,  0.0,
@@ -34,37 +36,62 @@ X.object2 = function() {
              1.0, -1.0,  0.0
         ]);
 
+  this._color = [1,0,1];
+
+  this._attributes = {};
+
+  this._uniforms = {};
+
+  this._type = goog.webgl.TRIANGLES;
+
 };
 // enable events
-X.__extends__(X.object2, X.base);
+X.__extends__(X.object, X.base);
 
-X.object2.prototype.init_shaders = function(gl) {
+X.object.prototype.init = function(gl) {
+
+  this._gl = gl;
+
+  var vertex_shader = X.shader.VERTEX;
 
   var fragment_shader = X.shader.FRAGMENT;
-  //fragment_shader = fragment_shader.replace('// {UNIFORMS}', 'uniform vec3 uColor;');
-  fragment_shader = fragment_shader.replace('// {MAIN_END}', 'gl_FragColor = vec4(0.0,0.0,0.0,1.0);');
+  fragment_shader = fragment_shader.replace('// {UNIFORMS}', 'uniform vec3 uColor;');
+  fragment_shader = fragment_shader.replace('// {MAIN_END}', 'gl_FragColor = vec4(uColor,1.0);');
 
+  var shader_program = X.shader.create(gl, vertex_shader, fragment_shader);
 
-  this.shader_program = X.shader.create(gl, X.shader.VERTEX, fragment_shader);
+  this._attributes['aVertexPosition'] = gl.getAttribLocation(shader_program, 'aVertexPosition');
+  this._uniforms['uColor'] = gl.getUniformLocation(shader_program, 'uColor');
 
-  return this.shader_program;
+  this.update();
 
-};
-
-X.object2.prototype.init_buffers = function(gl) {
-
-  this._vertex_buffer = X.buffer.create(gl, this._points);
+  return shader_program;
 
 };
 
-X.object2.prototype.render = function(gl) {
+X.object.prototype.update = function() {
 
-  X.buffer.bind(gl, this.shader_program, 'aVertexPosition', this._vertex_buffer, 3);
-
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  this._vertex_buffer = this._gl.create_buffer(this._points);
 
 };
 
+X.object.prototype.render = function() {
 
+  var gl = this._gl;
 
-goog.exportSymbol('X.object2', X.object2);
+  gl.attribute(this._attributes['aVertexPosition'], this._vertex_buffer, 3);
+  gl.uniform3fv(this._uniforms['uColor'], this._color);
+
+  gl.drawArrays(this._type, 0, 3);
+
+};
+
+X.object.prototype.destroy = function() {
+
+};
+
+goog.exportSymbol('X.object', X.object);
+goog.exportSymbol('X.object.prototype.init', X.object.prototype.init);
+goog.exportSymbol('X.object.prototype.update', X.object.prototype.update);
+goog.exportSymbol('X.object.prototype.render', X.object.prototype.render);
+goog.exportSymbol('X.object.prototype.destroy', X.object.prototype.destroy);
