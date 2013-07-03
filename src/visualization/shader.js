eb61588d5e09c162798d7a3c8174ab719a4e1b16
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2013-07-03 16:47:57 -0400

    Add the new X.shader which compiles shaders from chunks and gives full flexibility for editing.

--- src/visualization/shader.js
+++ src/visualization/shader.js
@@ -4,13 +4,68 @@ goog.provide('X.shader');
 // requires
 goog.require('goog.webgl');
 
+X.shader = function() {
+
+  // call the superclass constructor
+  X.__super__(this);
+
+  //
+  // class attributes
+
+  /**
+   * The classname of this class.
+   *
+   * @type {!string}
+   * @protected
+   */
+  this._classname = 'shader';
+
+  this._source = '';
+
+
+};
+X.__extend__(X.shader, X.base);
+
+X.shader.prototype.add_header = function(lines) {
+
+};
+
+X.shader.prototype.add _uniforms = function(lines) {
+
+  this._source = this._source.replace('// {UNIFORMS}', lines.join('\n'));
+
+};
+
+X.shader.prototype.add_varying = function(lines) {
+
+  this._source = this._source.replace('// {VARYING}', lines.join('\n'));
+
+};
+
+X.shader.prototype.add_code = function(lines) {
+
+  this._source = this._source.replace('// {CODE}', lines.join('\n'));
+
+};
+
+X.shader.prototype.add_code_at_main_begin = function(lines) {
+
+  this._source = this._source.replace('// {MAIN_BEGIN}', lines.join('\n'));
+
+};
+
+X.shader.prototype.add_code_at_main_end = function(lines) {
+
+  this._source = this._source.replace('// {MAIN_END}', lines.join('\n'));
+
+};
 
 X.shader.VERTEX = ['precision mediump float;',
                    'attribute vec3 aVertexPosition;',
                    '// {ATTRIBUTES}',
                    '// {UNIFORMS}',
                    '// {VARYING}',
-                   '// {FUNCTIONS}',
+                   '// {CODE}',
                    'void main(void) {',
                      '// {MAIN_BEGIN}',
                      'gl_Position = vec4(aVertexPosition, 1.0);',
@@ -20,7 +75,7 @@ X.shader.VERTEX = ['precision mediump float;',
 X.shader.FRAGMENT = ['precision mediump float;',
                      '// {UNIFORMS}',
                      '// {VARYING}',
-                     '// {FUNCTIONS}',
+                     '// {CODE}',
                      'void main(void) {',
                        '// {MAIN_BEGIN}',
                        'gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);',
@@ -38,7 +93,7 @@ X.shader.compile_ = function(gl, shader, type) {
   gl.compileShader(gl_shader);
 
   if (!gl.getShaderParameter(gl_shader, goog.webgl.COMPILE_STATUS)) {
-    throw new Error(gl.getShaderInfoLog(gl_fragment_shader));
+    throw new Error(gl.getShaderInfoLog(gl_shader));
   }
 
   return gl_shader;
