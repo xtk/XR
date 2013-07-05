792debae982a628a13eac4535a93017ecefe9a39
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2013-07-05 14:50:42 -0400

    Add X.camera and X.camera3D.

---
+++ src/visualization/camera.js
@@ -0,0 +1,89 @@
+// provides
+goog.provide('X.camera');
+
+// requires
+goog.require('X.base');
+goog.require('X.matrix');
+goog.require('X.vector');
+
+X.camera = function(width, height) {
+
+  // validate width and height
+  if (!goog.isNumber(width) || !goog.isNumber(height)) {
+
+    throw new Error('A camera needs valid width and height values.');
+
+  }
+
+  //
+  // call the standard constructor of X.base
+  X.__super__(this);
+
+  //
+  // class attributes
+
+  /**
+   * @inheritDoc
+   * @const
+   */
+  this._classname = 'camera';
+
+  /**
+   * The position of this camera, by default 0, 100, 0.
+   *
+   * @type {!X.vector}
+   * @protected
+   */
+  this._position = new X.vector(0, 100, 0);
+
+  /**
+   * The focus point of this camera, by default 0, 0, 0.
+   *
+   * @type {!X.vector}
+   * @protected
+   */
+  this._focus = new X.vector(0, 0, 0);
+
+  /**
+   * The unit vector pointing to the top of the three-dimensional space.
+   *
+   * @type {!X.vector}
+   * @protected
+   */
+  this._up = new X.vector(0, 0, 1);
+
+  /**
+   * The view matrix.
+   *
+   * @type {!Float32Array}
+   * @protected
+   */
+  this._view = X.matrix.makeLookAt(X.matrix.identity(), this._position, this._focus, this._up);
+
+};
+// inherit from X.base
+X.__extends__(X.camera, X.base);
+
+X.camera.prototype.zoom = function(value) {
+
+  this._view[14] += value;
+
+};
+
+X.camera.prototype.rotate = function(x,y) {
+
+  // in radii, the 5 is a constant stating how quick the rotation performs..
+  var angleX = -x / 5 * Math.PI / 180;
+  var angleY = -y / 5 * Math.PI / 180;
+
+  // we need to normalize the axis here
+  var axisX = new X.vector(this._view[1], this._view[5], this._view[9]);
+  var axisY = new X.vector(this._view[0], this._view[4], this._view[8]);
+  axisX.normalize();
+  axisY.normalize();
+
+  // row+col * 4
+  X.matrix.rotate(this._view, angleX, axisX.x, axisX.y, axisX.z);
+  X.matrix.rotate(this._view, angleY, axisY.x, axisY.y, axisY.z);
+
+};
---
+++ src/visualization/camera3D.js
@@ -0,0 +1,44 @@
+// provides
+goog.provide('X.camera3D');
+
+// requires
+goog.require('X.camera');
+
+X.camera3D = function(width, height) {
+
+  //
+  // call the standard constructor of X.base
+  X.__super__(this, width, height);
+
+  //
+  // class attributes
+
+  /**
+   * @inheritDoc
+   * @const
+   */
+  this._classname = 'camera3D';
+
+  /**
+   * The field of view in degrees.
+   *
+   * @type {!number}
+   * @const
+   */
+  this._field_of_view = 45;
+
+  this._near = 1;
+
+  this._far = 10000;
+
+  /**
+   * The perspective matrix.
+   *
+   * @type {Array.<number>|Float32Array|Float64Array|null}
+   * @protected
+   */
+  this._perspective = X.matrix.makePerspective(X.matrix.identity(), this._field_of_view, (width/height), this._near, this._far);
+
+};
+// inherit from X.base
+X.__extends__(X.camera3D, X.camera);
